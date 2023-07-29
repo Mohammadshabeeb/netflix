371eccaba76a81f55e00243d6dc0e171b3ebb083
mohammadshabeeb <mohammadshabeeb500@gmail.com>
2023-07-29 09:23:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import NavBar from './Components/Navbar/NavBar';
+import {  originals } from './Url'
+import { action } from './Url'
+import Banner from './Components/Banner/Banner';
+import Rowpost from './Components/Rowpost/Rowpost';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavBar/>
+     <Banner/>
+     <Rowpost url={originals} title="Netflix originals"/>
+     <Rowpost url={action} title="Action" isSmall/>
+     {/* <Rowpost url={documents} title="Documents" isSmall/> */}
     </div>
   );
 }
